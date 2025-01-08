3e7c07981ca088e7aaa37faf271bfa93c3158d36
Antoine Provot <antoine@sandboxquantum.com>
2025-01-08 09:10:26 +0100

    Solution mix column

--- aes.py
+++ aes.py
@@ -57,13 +57,37 @@ def inv_shift_rows(s):
 def add_round_key(s, k):
     return s # TODO :p
 
+# learned from https://web.archive.org/web/20100626212235/http://cs.ucsb.edu/~koc/cs178/projects/JT/aes.c
+xtime = lambda a: (((a << 1) ^ 0x1B) & 0xFF) if (a & 0x80) else (a << 1)
+
+
+def mix_single_column(a):
+    # see Sec 4.1.2 in The Design of Rijndael
+    t = a[0] ^ a[1] ^ a[2] ^ a[3]
+    u = a[0]
+    a[0] ^= t ^ xtime(a[0] ^ a[1])
+    a[1] ^= t ^ xtime(a[1] ^ a[2])
+    a[2] ^= t ^ xtime(a[2] ^ a[3])
+    a[3] ^= t ^ xtime(a[3] ^ u)
+
+
 def mix_columns(s):
-    # see Sec 4.1.3 in The Design of Rijndael
-    return s # TODO :p
+    for i in range(4):
+        mix_single_column(s[i])
+
 
 def inv_mix_columns(s):
     # see Sec 4.1.3 in The Design of Rijndael
-    return s # TODO :p
+    for i in range(4):
+        u = xtime(xtime(s[i][0] ^ s[i][2]))
+        v = xtime(xtime(s[i][1] ^ s[i][3]))
+        s[i][0] ^= u
+        s[i][1] ^= v
+        s[i][2] ^= u
+        s[i][3] ^= v
+
+    mix_columns(s)
+    
 
 def bytes2matrix(text):
     return None # TODO :p
